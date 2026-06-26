41173bfbf53717fe3d55b51eae70b753707e9006
Rubn09 <rubentavares2004@gmail.com>
2026-06-26 20:03:06 +0100

    Still EF Core Configuration

--- api/ProductApi/Program.cs
+++ api/ProductApi/Program.cs
@@ -1,4 +1,11 @@
+using ProductApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Services
 builder.Services.AddControllers();
