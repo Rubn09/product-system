b44e1bcc0bbdf858937250a952e568650e8b98b6
Rubn09 <rubentavares2004@gmail.com>
2026-06-27 15:12:26 +0100

    dockey api swagger update

--- api/ProductApi/Program.cs
+++ api/ProductApi/Program.cs
@@ -15,11 +15,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Pipeline
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
