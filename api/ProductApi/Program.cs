80e9cc95b2c7da7a8609012274f09b04c525f063
Rubn09 <rubentavares2004@gmail.com>
2026-06-29 20:31:51 +0100

    add database migration on application startup

--- api/ProductApi/Program.cs
+++ api/ProductApi/Program.cs
@@ -23,6 +23,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Pipeline
 
     app.UseSwagger();
