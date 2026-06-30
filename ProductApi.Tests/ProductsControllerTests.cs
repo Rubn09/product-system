adae82dafd282ecacd29c42273c286f5ba8c538c
Rubn09 <rubentavares2004@gmail.com>
2026-06-30 19:48:06 +0100

    test all endpoints

--- ProductApi.Tests/ProductsControllerTests.cs
+++ ProductApi.Tests/ProductsControllerTests.cs
@@ -36,4 +36,116 @@ public class ProductsControllerTests
 
         products.Should().HaveCount(2);
     }
+
+    [Fact]
+    public async Task GetById_Should_Return_Product_When_Exists()
+    {
+        var mockService = new Mock<IProductService>();
+
+        mockService.Setup(s => s.GetByIdAsync(1))
+            .ReturnsAsync(new Product { Id = 1, Name = "PC", Price = 1000 });
+
+        var controller = new ProductsController(mockService.Object);
+
+        var result = await controller.GetById(1);
+
+        var ok = result as OkObjectResult;
+
+        ok.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task GetById_Should_Return_NotFound_When_Missing()
+    {
+        var mockService = new Mock<IProductService>();
+
+        mockService.Setup(s => s.GetByIdAsync(1))
+            .ReturnsAsync((Product?)null);
+
+        var controller = new ProductsController(mockService.Object);
+
+        var result = await controller.GetById(1);
+
+        result.Should().BeOfType<NotFoundResult>();
+    }
+
+    [Fact]
+    public async Task Create_Should_Return_CreatedProduct()
+    {
+        var mockService = new Mock<IProductService>();
+
+        var product = new Product { Id = 1, Name = "PC", Price = 1000 };
+
+        mockService.Setup(s => s.CreateAsync(product))
+            .ReturnsAsync(product);
+
+        var controller = new ProductsController(mockService.Object);
+
+        var result = await controller.Create(product);
+
+        var created = result as CreatedAtActionResult;
+
+        created.Should().NotBeNull();
+        created!.Value.Should().Be(product);
+    }
+
+    [Fact]
+    public async Task Update_Should_Return_NoContent_When_Success()
+    {
+        var mockService = new Mock<IProductService>();
+
+        mockService.Setup(s => s.UpdateAsync(1, It.IsAny<Product>()))
+            .ReturnsAsync(true);
+
+        var controller = new ProductsController(mockService.Object);
+
+        var result = await controller.Update(1, new Product());
+
+        result.Should().BeOfType<NoContentResult>();
+    }
+
+    [Fact]
+    public async Task Update_Should_Return_NotFound_When_Fails()
+    {
+        var mockService = new Mock<IProductService>();
+
+        mockService.Setup(s => s.UpdateAsync(1, It.IsAny<Product>()))
+            .ReturnsAsync(false);
+
+        var controller = new ProductsController(mockService.Object);
+
+        var result = await controller.Update(1, new Product());
+
+        result.Should().BeOfType<NotFoundResult>();
+    }
+
+    [Fact]
+    public async Task Delete_Should_Return_NoContent_When_Success()
+    {
+        var mockService = new Mock<IProductService>();
+
+        mockService.Setup(s => s.DeleteAsync(1))
+            .ReturnsAsync(true);
+
+        var controller = new ProductsController(mockService.Object);
+
+        var result = await controller.Delete(1);
+
+        result.Should().BeOfType<NoContentResult>();
+    }
+
+    [Fact]
+    public async Task Delete_Should_Return_NotFound_When_Fails()
+    {
+        var mockService = new Mock<IProductService>();
+
+        mockService.Setup(s => s.DeleteAsync(1))
+            .ReturnsAsync(false);
+
+        var controller = new ProductsController(mockService.Object);
+
+        var result = await controller.Delete(1);
+
+        result.Should().BeOfType<NotFoundResult>();
+    }
 }
--- api/ProductApi/obj/Debug/net10.0/ProductApi.AssemblyInfo.cs
+++ api/ProductApi/obj/Debug/net10.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9eebcd41a45221a541bc9476ab91de41bdfcadc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a3a1406d9db2d225da45794a269f867b7384bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
